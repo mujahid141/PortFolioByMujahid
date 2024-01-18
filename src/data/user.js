670d6337f934db2924f98cbb59589f1a0f256d43
Mujahid Anwar <mujahidanwar141@gmail.com>
2024-01-18 19:48:01 +0500

    Imformation completed

--- src/data/user.js
+++ src/data/user.js
@@ -16,13 +16,13 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
+		title: "Full-stack web and mobile app developer, and student of Human Nature.",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a backend developer with expertise in Pytho Framework Django. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I’m Mujahid Anwar , living in Pakistan Islamabad, where I design the future.",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
@@ -35,48 +35,48 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Project 1",
+			title: "Game__Hub",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Game hub is Frontend based project using React 18 + Typescript, Furthermore I have used multiple typecript concepts for details Check my Github",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/typescript/typescript.png",
+			linkText: "View Project",
+			link: "https://github.com/mujahid141/game-hub",
+		},
+
+		{
+			title: "Movies Search",
+			description:
+				"FilmFind is your go-to movie search app built on the robust React framework. Explore detailed movie information, watch trailers, and make your movie nights unforgettable. FilmFind, where React meets cinematic delight. Download now!",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/mujahid141/moiveshub",
 		},
 
 		{
-			title: "Project 2",
+			title: "weathe App",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 3",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Stay ahead of the elements with WeatherWave – a cutting-edge weather app powered by React. Effortlessly check current conditions, hourly forecasts, and 7-day outlooks.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/mujahid141/weatherAPP",
 		},
 
 		{
-			title: "Project 4",
+			title: "To-Do APP",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"ReactTask: Your ultimate task management solution. Streamlined, intuitive, and real-time. Enjoy drag-and-drop organization, customizable categories, and responsive design. Never miss a deadline with reminders and notifications. Experience productivity with a modern twist – download ReactTask now.",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/mujahid141/todo-app-main",
 		},
 
 		{
-			title: "Project 5",
+			title: "BMI Calculator",
 			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+				"Want to Find Yous BMI, I got you Here is Simple and Easy BMI Finder",
 			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
 			linkText: "View Project",
-			link: "https://github.com",
+			link: "https://github.com/mujahid141/todo-app-main",
 		},
 	],
 };
